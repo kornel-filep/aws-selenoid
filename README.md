27d5f2a307ad9ddd0094e7ef85194b7ca5a84dd2
kfilep <kornel.filep@logmein.com>
2021-06-26 12:09:50 +0300

    added dotenv and basic cred check on startup

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 This is a small demo project to showcase how to use Selenoid with AWS EC2 instances.
 
 Selenoid: https://github.com/aerokube/selenoid
+
+# Setup
+
+1. Install latest NodeJS LTS.
+2. Configure AWS credentials as shown in `.env.example`.
+
+# Run
+
+1. `yarn watch` to start TSC in watch mode.
+2. `yarn start` to execute the project after starting `yarn watch`.
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import dotenv from 'dotenv'
+import AWS from 'aws-sdk'
+
+dotenv.config()
+
+AWS.config.getCredentials((err) => {
+        if (err){
+            console.log("Your AWS credentials might not be configured. Please configure them through environment variables using dotenv.")
+            console.log(err.stack);
+        }
+        else {
+            console.log("AWS credentials configuration found");
+        }
+});
