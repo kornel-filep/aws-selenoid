c382eefba644ad034e88cba85b8bcb73d3fc51b0
kfilep <kornel.filep@logmein.com>
2021-06-26 19:52:26 +0200

    added initial script to create EC2 instance with selenoid

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import dotenv from 'dotenv'
 import AWS from 'aws-sdk'
+import { SelenoidInstallSteps } from './selenoid/SelenoidInstallSteps'
 
 dotenv.config()
+AWS.config.update({ region: 'eu-north-1' })
+
+const EC2 = new AWS.EC2()
+const selenoidInstallSteps = new SelenoidInstallSteps()
 
 AWS.config.getCredentials((err) => {
     if (err) {
@@ -13,3 +18,24 @@ AWS.config.getCredentials((err) => {
         console.log('AWS credentials configuration found')
     }
 })
+
+const params = {
+    ImageId: process.env['AWS_AMI_ID'],
+    InstanceType: 't3.micro',
+    KeyName: process.env['AWS_KEY_NAME'],
+    SecurityGroupIds: [process.env['AWS_SECURITY_GROUP_ID']],
+    UserData: Buffer.from(selenoidInstallSteps.getInstallSteps('chrome').join('\n')).toString('base64'),
+    MaxCount: 1,
+    MinCount: 1,
+} as AWS.EC2.RunInstancesRequest
+
+;(async () => {
+    const instances = await EC2.runInstances(params).promise()
+    const createdInstance = await EC2.waitFor('instanceStatusOk', {
+        InstanceIds: [instances.Instances![0].InstanceId!],
+    }).promise()
+    const describedInstance = await EC2.describeInstances({
+        InstanceIds: [createdInstance!.InstanceStatuses![0].InstanceId!],
+    }).promise()
+    console.log(describedInstance.Reservations![0].Instances![0].PublicIpAddress)
+})()
--- src/selenoid/SelenoidInstallSteps.ts
+++ src/selenoid/SelenoidInstallSteps.ts
@@ -1,13 +1,16 @@
 export class SelenoidInstallSteps {
     private _installSteps = [
-        'mkdir selenoid',
-        'cd selenoid/',
-        'sudo curl -L "https://github.com/aerokube/cm/releases/download/1.8.1/cm_linux_amd64" --output configmanager',
-        'sudo chmod +x configmanager',
-        './configmanager selenoid start',
+        '#!/bin/bash',
+        'mkdir /home/ubuntu/selenoid',
+        'sudo curl -L "https://github.com/aerokube/cm/releases/download/1.8.1/cm_linux_amd64" --output /home/ubuntu/selenoid/configmanager',
+        'sudo chmod +x /home/ubuntu/selenoid/configmanager',
     ]
 
-    public get installSteps() {
+    public getInstallSteps(browser?: string) {
+        if (browser) {
+            this._installSteps.push(`/home/ubuntu/selenoid/configmanager selenoid start --browsers '${browser}'`)
+        }
+        this._installSteps.push('/home/ubuntu/selenoid/configmanager selenoid start')
         return this._installSteps
     }
 }
