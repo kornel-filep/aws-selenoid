3f2fe9c4a547672dced85480faf60f3a372a5219
kfilep <kornel.filep@logmein.com>
2021-06-26 12:19:15 +0300

    formatting

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,12 @@ import AWS from 'aws-sdk'
 dotenv.config()
 
 AWS.config.getCredentials((err) => {
-        if (err){
-            console.log("Your AWS credentials might not be configured. Please configure them through environment variables using dotenv.")
-            console.log(err.stack);
-        }
-        else {
-            console.log("AWS credentials configuration found");
-        }
-});
+    if (err) {
+        console.log(
+            'Your AWS credentials might not be configured. Please configure them through environment variables using dotenv.'
+        )
+        console.log(err.stack)
+    } else {
+        console.log('AWS credentials configuration found')
+    }
+})
